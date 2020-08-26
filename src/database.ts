17d4ad75b7c857a78d461a51462501f982ff7039
Jorge Medina <jorge.si.med@gmail.com>
2020-08-25 20:38:54 -0500

    config connection to mongodb

---
+++ src/database.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const connect = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost/ts-app-tutorial', {
+            useNewUrlParser: true
+        })
+        console.log(">>> Database connected");
+        
+    } catch (error) {
+        console.log('Error bro');
+        
+    }
+}
+
+export default connect
