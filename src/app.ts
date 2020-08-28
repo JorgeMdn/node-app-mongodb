581e93e1732cde390a938859f6c244b76bfb5c81
Jorge Medina <jorge.si.med@gmail.com>
2020-08-27 22:24:44 -0500

    add configuration server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express from "express";
-import morgan from 'morgan'
-//import exphbs from 'express-handlebars'
+import morgan from 'morgan' // middleware
+import exphbs from 'express-handlebars'
+import path from 'path'
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+// Routes
+import indexRoutes from "./routes";
+import  tasksRoutes from "./routes/tasks";
 class Application {
     app: express.Application
     constructor(){
@@ -10,23 +16,41 @@ class Application {
         this.routes()
     }
 
-    start = () => {
-        this.app.listen(3000, () => {
-            console.log('Server running');
-        })
-    }
-
     settings = () => {
-    
+        this.app.set('port', 3000)
+        // unir o encontrar carpeta views en el proyecto
+        this.app.set('views', path.join(__dirname, 'views'))
+        // configuracion de plantillas 
+        this.app.engine('hbs', exphbs({
+            layoutsDir: path.join(this.app.get('views'), 'layouts'),
+            partialsDir: path.join(this.app.get('views'), 'partials'),
+            defaultLayout: 'main',
+            extname: '.hbs',
+            handlebars: allowInsecurePrototypeAccess(Handlebars)
+        }))
+        this.app.set('view engine', '.hbs')
     }
 
     middlewares = () => {
         this.app.use(morgan('dev'))
+        // Recibiendo cualquier tipo de dato
+        this.app.use(express.json()) // recibir json - api
+        this.app.use(express.urlencoded({extended: false})) // entender formulario
     }
 
     routes = () => {
-    
+        this.app.use(indexRoutes)
+        this.app.use('/tasks',tasksRoutes)
+        this.app.use(express.static(path.join(__dirname, 'public')))
+    }
+
+    start = () => {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server on port', this.app.get('port'));
+        })
     }
 }
 
 export default Application
+            
+            
