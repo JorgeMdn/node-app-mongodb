5fbee68d810813ddab7efbaabe35f82b27283af2
Jorge Medina <jorge.si.med@gmail.com>
2020-08-27 22:25:27 -0500

    resolve error with mongo, warning

--- src/database.ts
+++ src/database.ts
@@ -3,9 +3,10 @@ import mongoose from 'mongoose'
 const connect = async () => {
     try {
         await mongoose.connect('mongodb://localhost/ts-app-tutorial', {
-            useNewUrlParser: true
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         })
-        console.log(">>> Database connected");
+        console.log(">>> Database connected.");
         
     } catch (error) {
         console.log('Error bro');
